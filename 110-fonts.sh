ab00210a2e6083ca8ecdab0caa9a8bea91be7e83
Seth Dawson <seth.dawson@hotmail.com>
2021-01-09 20:15:07 -0600

    added font script

---
+++ 110-fonts.sh
@@ -0,0 +1,106 @@
+##!/bin/bash
+#set -e
+#
+##########################################################
+#                                                        #
+#              SMD-Linux Install Script                  #
+#  SMD-Linux Automatic Installation Script               #
+#  Inspired and Forked From                              #
+#  https://github.com/SofianeHamlaoui/ArchI0             #
+#  And                                                   #
+#  https://github.com/arcolinuxd/arco-xfce               #
+##########################################################
+#  +FIRST  : sudo chmod +x 110-fonts.sh                  #
+#                (Give EXEC Access To Script)            #
+#  +TO RUN    : ./arch-xfce.sh                           #
+##########################################################
+#                                                        #
+#                DO NOT JUST RUN THIS.                   #
+#       EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.         #
+#                                                        #
+##########################################################
+#
+#
+#  TO DO:
+#
+#
+######################## Variables #######################
+#
+# b=bold u=underline bl=black r=red g=green
+# y=yellow bu=blue m=magenta c=cyan w=white
+# endc=end-color enda=end-argument
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+y='\E[33m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+version="2.0.0-1"
+
+####################### Functions ########################
+
+function install {
+  # Check If Package Is Installed
+  if pacman -Qi $1 &> /dev/null; then
+      echo -e " [${g}✔${endc}]::[${b}"$1"${enda}] Is Already Installed!"
+  else
+      sudo pacman -S --noconfirm --needed $1
+      # Verify Successful Installation
+    if pacman -Qi $package &> /dev/null; then
+      echo -e " [${g}✔${endc}]::[${b}"$1"${enda}]: Installed!"
+    else
+      echo -e " [${r}!${endc}]::[${b}"$1"${enda}]: ${r}NOT Installed!${endc}"
+    fi
+  fi
+}
+
+function category {
+	echo -e " [${g}✔${endc}]::Installing Software For Category ${b}${bu}["$1"]${endc}${enda}"
+	echo
+}
+
+function end_category {
+	echo
+    echo -e " [${g}✔${endc}]::Category ${bu}${b}["$1"]${enda}${endc} Installed!"
+    echo
+}
+
+###############################################################################
+
+category Fonts
+
+list=(
+smdlinux-fonts-git
+awesome-terminal-fonts
+adobe-source-sans-pro-fonts
+cantarell-fonts
+noto-fonts
+ttf-bitstream-vera
+ttf-dejavu
+ttf-droid
+ttf-hack
+ttf-inconsolata
+ttf-liberation
+ttf-roboto
+ttf-ubuntu-font-family
+tamsyn-font
+)
+
+count=0
+
+for name in "${list[@]}" ; do
+  count=$[count+1]
+  echo -e " ${y}Installing package # "$count" ${b}["$name"]${enda} ${endc}" ;
+  install $name
+done
+
+###############################################################################
+
+end_category Fonts
+echo -e " ${bu}SMD-Linux Fonts Installation Script Version${b} $version ${enda} ${endc}"
